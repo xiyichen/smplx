92b815fa3a3dc4dca978c180180305f7af844565
Xiyi Chen <50854238+xiyichen@users.noreply.github.com>
2023-07-16 13:56:00 +0800

    Update utils.py

--- smplx/utils.py
+++ smplx/utils.py
@@ -31,6 +31,7 @@ class ModelOutput:
     global_orient: Optional[Tensor] = None
     transl: Optional[Tensor] = None
     v_shaped: Optional[Tensor] = None
+    bone_transforms: Optional[Tensor] = None
 
     def __getitem__(self, key):
         return getattr(self, key)
